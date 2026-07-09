d899082d7311b979313bf1c652c26ca0a59b7213
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-09 16:04:00 +0500

    Update jobs.js

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -20,16 +20,51 @@ const getSingleJob = async (req, res) => {
     user: { userId },
     params: { id: jobId },
   } = req;
+  const job = await Job.findOne({ createdBy: userId, _id: jobId });
+
   if (!job) {
     throw new NotFound(`No job found with id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
-const updateJob = (req, res) => {
-  res.send('Update Job');
+const updateJob = async (req, res) => {
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === '' || position === '') {
+    throw new BadRequest('Company or Position field is empty');
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { createdBy: userId, _id: jobId },
+    req.body,
+    { runValidators: true, new: true },
+  );
+
+  if (!job) {
+    throw new NotFound(`No job found with id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
-const deleteJob = (req, res) => {
-  res.send('Delete Job');
+const deleteJob = async (req, res) => {
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+  const job = await Job.findOneAndDelete({ createdBy: userId, _id: jobId });
+
+  if (!job) {
+    throw new NotFound(`No job found with id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = { createJob, getAllJobs, getSingleJob, updateJob, deleteJob };
