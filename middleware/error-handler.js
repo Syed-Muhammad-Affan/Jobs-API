a4f51f829f69d1042dcd3d21be11e687b4a6bcf3
Syed Muhammad Affan <aaffaann1122ee@gmail.com>
2026-07-10 15:04:01 +0500

    error handling of mongo db errors

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -2,11 +2,38 @@ const { CustomAPIError } = require('../errors');
 const { StatusCodes } = require('http-status-codes');
 
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  const customError = {
+    statuscode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wrong try again later',
+  };
+
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+
+    customError.statuscode = 400;
   }
 
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: err.msg });
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue,
+    )} field, please choose another value`;
+
+    customError.statuscode = 400;
+  }
+
+  if (err.name === 'CastError') {
+    customError.msg = `No item found with id: ${err.value}`;
+
+    customError.statuscode = 404;
+  }
+
+  return res.status(customError.statuscode).json({ msg: customError.msg });
 };
 
 module.exports = errorHandlerMiddleware;
